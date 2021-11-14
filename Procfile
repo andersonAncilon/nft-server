43bf0cb4c9592b05da5b5d9cec1e34b8dc296cdf
Anderson Ancilon dos Santos <andersonas@abcbrasil.com.br>
2021-11-14 08:03:26 -0300

    feat: enabling cors

--- Procfile
+++ Procfile
@@ -1,3 +1,1 @@
-
-   
 web: npm run start:prod
--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter(),
   );
+  app.enableCors();
 
   await app.listen(process.env.PORT || 3000);
 }
