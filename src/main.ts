ccc2b17b43ad0621fc6127da15fca43b88b7ce5d
Anderson Ancilon dos Santos <andersonas@abcbrasil.com.br>
2021-11-14 08:15:15 -0300

    fix: adding console to show what port is server running on

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
   );
   app.enableCors();
 
-  await app.listen(process.env.PORT || 3000);
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT, () => console.log(`Running por port: ${PORT}`));
 }
 bootstrap();
