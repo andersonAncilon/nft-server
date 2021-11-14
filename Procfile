09b8b1aabc6e8d072508e023554d4ae54c404430
Anderson Ancilon dos Santos <andersonas@abcbrasil.com.br>
2021-11-14 07:48:16 -0300

    feat: setting up fastify. Adding Procfile for heroku

---
+++ Procfile
@@ -0,0 +1,3 @@
+
+   
+web: npm run start:prod
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  NestFastifyApplication,
+  FastifyAdapter,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
